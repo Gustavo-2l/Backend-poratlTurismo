1b97f8a85a9ac114701054c59b02528450ec5964
Gustavo-2l <dri.khedira@gmail.com>
2025-07-21 15:13:20 -0300

    UPDATE

--- server.js
+++ server.js
@@ -1,19 +1,39 @@
-// Importa a instância do Sequelize configurada e o modelo de usuário
+
+
 const sequelize = require('./config/db');
+const userRoutes = require('./routes/userRoutes')
+
+
+
+
+
 require('dotenv').config();
 const express = require('express')
- 
-const userRoutes = require('./routes/userRoutes')
- 
+
+
+const cors = require('cors');
+
+
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173', // frontend (Vite + React)
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true // se precisar enviar cookies
+}));
  
 app.use(express.json());
  
 app.get('/', (req, res)=> res.send('api funcionando'))
+
+app.post('/api/register', (req, res) => {
+  const { nome, email, senha } = req.body;
+  console.log('Recebido:', nome, email, senha);
+  res.json({ mensagem: 'Usuário cadastrado com sucesso!' });
+});
  
 app.use('/api/users', userRoutes)
- 
-const PORT = process.env.PORT;
+
  
 sequelize.authenticate()
   .then(() => {
@@ -24,3 +44,9 @@ sequelize.authenticate()
     console.log('banco de dados sincronizado')
     app.listen(PORT, () => console.log("SERVIDOR RODANDO NA PORTA: " + PORT))
   }).catch(erro => console.log("Erro interno do servidor", erro))
+
+
+  const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
